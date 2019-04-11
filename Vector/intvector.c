73c6208f6ad5880ffc7ca8ca57f454d206ca417f
*Honshu <30958723+Visualieyes@users.noreply.github.com>
2019-04-11 01:45:38 -0700

    Update and rename intvector.c to Vector/intvector.c

--- Vector/intvector.c
+++ Vector/intvector.c
@@ -12,6 +12,7 @@
 #include "intvector.h"
 
 
+// Initialize and return a vector from an array of values
 IntVector ivecFromArray( int dimension, int data[] ) {
     int *vector = calloc(dimension, sizeof(int));
     int i;
@@ -24,8 +25,8 @@ IntVector ivecFromArray( int dimension, int data[] ) {
 }
 
 
-void ivecPrint(IntVector v) {
-    
+// Output vector
+void ivecPrint(IntVector v) {   
     int i;
 
     for(i=0; i<v.dim; i++){
@@ -36,40 +37,34 @@ void ivecPrint(IntVector v) {
 
 // Return true iff v1 has the same coordinate as v2
 // PRE: v1.dim == v2.dim
-bool ivecEqual( IntVector v1, IntVector v2 ){
-	
+bool ivecEqual( IntVector v1, IntVector v2 ){	
 	int i;
 	for(i=0; i<v1.dim; i++){
 		if (v1.vector[i] != v2.vector[i]){
 			return false;
 		}
 	}
-
 	return true;
 }
+
 
 // Return a new vector, v, where v = v1 + v2
 // PRE: v1.dim == v2.dim
 IntVector ivecAdd( IntVector v1, IntVector v2 ){
-
 	int n = v1.dim;
-
 	int sum[n];
 
 	int i;
-
 	for (i=0; i<n; i++){
 		sum[i] = v1.vector[i] + v2.vector[i];
 	}
-
 	return ivecFromArray(n, sum);
-
 }
+
 
 // Return a new vector, v, where v = v1 - v2
 // PRE: v1.dim == v2.dim
 IntVector ivecSubtract( IntVector v1, IntVector v2 ){
-
 	int n = v1.dim;
 	int sum[n];
 	int i;
@@ -77,9 +72,15 @@ IntVector ivecSubtract( IntVector v1, IntVector v2 ){
 	for (i=0; i<n; i++){
 		sum[i] = v1.vector[i] - v2.vector[i];
 	}
-
 	return ivecFromArray(n, sum);
+	
+}
 
+
+
+
+int main(){
+	return 0;
 }
 
 
